eca421cdb6d2dd3f54207f68e67ee4a4b8373470
Shogo Tobori <qb.brady.121212@gmail.com>
2020-07-21 15:34:46 +0000

    [Add]ログイン機能・画像投稿機能作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '95d41babf58c04e2f0993caba6caed829edbdfd3bbe52ffe42eb971e2539363d98f34651415e1a74a7c5ff931fb0cc497bb835d7a77215b87a0e0572763df507'
