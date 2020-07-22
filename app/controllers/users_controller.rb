61c836046a07e5f084078b87776c39b5a8c20461
Shogo Tobori <qb.brady.121212@gmail.com>
2020-07-22 14:05:53 +0000

    [Finish]プロフィール画像をビューに設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
 
   def edit
   	@user = User.find(params[:id])
+  end
 
   def update
   	@user = User.find(params[:id])
@@ -13,8 +14,8 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
-private
-  def user_params
+  private
+   def user_params
     params.require(:user).permit(:name, :profile_image)
+   end
   end
-end
